51de931d0a800d4e8d167c30c7faad04cce3f9c5
Kuanlin-Chen <webb1992810625@gmail.com>
2025-12-17 16:05:35 +0100

    Revert "Update to https" since a certificate is needed
    This reverts commit 0d8a21dfcc67d7f76d514dcc57a616490ed9fe5d.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "alb_url" {
   description = "The URL of the Application Load Balancer"
-  value       = "https://${aws_lb.load_balancer.dns_name}"
+  value       = "http://${aws_lb.load_balancer.dns_name}"
 }
